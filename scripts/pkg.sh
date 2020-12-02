ee9cc4b05b097cfe379e64070f1bf91491f2eb1b
emmett1 <emmett1.2miligrams@gmail.com>
2020-12-02 21:42:55 +0800

    fix https://github.com/emmett1/alicelinux/issues/4

--- scripts/pkg.sh
+++ scripts/pkg.sh
@@ -12,13 +12,28 @@ else
 fi
 
 name=pkg
-version=20200712
+version=20201202
 
 cd $SRC
 
 install -Dm644 $FILES_DIR/functions $PKG/var/lib/pkg/functions
 install -Dm644 $FILES_DIR/pkg.conf $PKG/etc/pkg.conf
 install -Dm755 $FILES_DIR/pkg $PKG/bin/pkg
+
+install -d $PKG/etc
+cat > $PKG/etc/pkg.conf << EOF
+# configuration file for pkg
+
+export CFLAGS="$CFLAGS"
+export CXXFLAGS="\${CFLAGS}"
+export MAKEFLAGS="$MAKEFLAGS"
+
+#SOURCE_DIR="/var/lib/pkg/sources"
+#WORK_DIR="/var/lib/pkg/work"
+#LOG_DIR="/var/lib/pkg/log"
+#FILES_DIR="/var/lib/pkg/files"
+#SCRIPTS_DIR="/var/lib/pkg/scripts"
+EOF
 
 xinstall
 
